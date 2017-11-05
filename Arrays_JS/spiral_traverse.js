5885d330ffe49f256e8bfa1945d13d54ea284c59
Jimmy Li <jimmyli921@gmail.com>
2017-11-04 18:37:28 -0700

    Add spiral traverse 2

--- Arrays_JS/spiral_traverse.js
+++ Arrays_JS/spiral_traverse.js
@@ -31,4 +31,22 @@ const snail = function(array) {
   return arr;
 };
 
+const snail2 = function(array) {
+  var result;
+  while (array.length) {
+    // Steal the first row.
+    result = (result ? result.concat(array.shift()) : array.shift());
+    // Steal the right items.
+    for (let i = 0; i < array.length; i++)
+      result.push(array[i].pop());
+    // Steal the bottom row.
+    result = result.concat((array.pop() || []).reverse());
+    // Steal the left items.
+    for (let i = array.length - 1; i >= 0; i--)
+      result.push(array[i].shift());
+  }
+  return result;
+};
+
 console.log(snail([[1,2,3],[4,5,6],[7,8,9]]));
+console.log(snail2([[1,2,3],[4,5,6],[7,8,9]]));
